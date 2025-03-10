4d3a17569130b4fe9e106d38d1d0834e30426b65
Jiramath17 <40719@prot.ac.th>
2025-03-10 20:58:39 +0700

    ex01

---
+++ cell04/ex01/age.py
@@ -0,0 +1,12 @@
+
+age = int(input("กรุณาบอกอายุของคุณ: "))
+
+# ใช้ฟังก์ชันในการคำนวณอายุในอนาคต
+def future_age(current_age, years):
+    return current_age + years
+
+# แสดงอายุปัจจุบันและอายุในอนาคต
+print(f"ตอนนี้คุณอายุ {age} ปี.")
+print(f"ในอีก 10 ปี คุณจะอายุ {future_age(age, 10)} ปี.")
+print(f"ในอีก 20 ปี คุณจะอายุ {future_age(age, 20)} ปี.")
+print(f"ในอีก 30 ปี คุณจะอายุ {future_age(age, 30)} ปี.")
