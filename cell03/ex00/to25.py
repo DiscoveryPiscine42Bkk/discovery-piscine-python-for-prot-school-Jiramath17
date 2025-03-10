62de7faa9c5d028f1e004a1d08564e68cec51d59
Jiramath17 <40719@prot.ac.th>
2025-03-10 20:30:19 +0700

    ex00

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,12 @@
+def main():
+    number = int(input("Enter a number less than 25\n"))
+    
+    if number > 25:
+        print("Error")
+    else:
+        while number <= 25:
+            print(f"Inside the loop, my variable is {number}")
+            number += 1
+
+if __name__ == "__main__":
+    main()
